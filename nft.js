a9558d5bf654c086f45b4a2b540f9bcdfbb7f7fe
RO=om <riidatibula@gmail.com>
2022-05-01 17:14:10 +0800

    create first art

--- nft.js
+++ nft.js
@@ -5,10 +5,11 @@ function setup() {
     createCanvas(800, 800, SVG)
 
     PALETTE = [
-        color(255, 52, 154),
-        color(4, 0, 152),
-        'limegreen',
-        'teal'
+        '#003049',
+        '#d62828',
+        '#f77f00',
+        '#fcbf49',
+        '#eae2b7'
     ]
 
     noLoop()
@@ -17,17 +18,29 @@ function setup() {
 }
 
 function draw() {
-    drawOutline()
 
-    push()
-        translate(width/2, height/2)
-        // tripleCircle(0, 0, 100, PALETTE)
-        // tripleEquiTriangle(0, 0, 100, PALETTE)
-        denseCircle(0, 0, 300)
-    pop()
+    fill('#00296b')
+    // drawOutline()
+    noStroke()
+
+    const circle_size = 50
+    const hoz_circles = ART_SIZE / circle_size
+    const ver_circles = ART_SIZE / circle_size
+
+    let x = ((width-ART_SIZE)/2) + (circle_size/2)
+    let y = ((width-ART_SIZE)/2) + (circle_size/2)
+
+    for (j = 1; j <= ver_circles; j++) {
+        for (i = 1; i <= hoz_circles; i++) {
+            push()
+                tripleCircle(x*i, y*j, circle_size, PALETTE)
+            pop()
+        }
+    }
 }
 
 function drawOutline() {
+    strokeWeight(1)
     push()
         translate(width/2, height/2)
         rect(0, 0, ART_SIZE, ART_SIZE)
